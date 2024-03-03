fe981331d44b39b3d9a91ea0473200be141388b0
Pedro Antonny <pedro.antonio.1597@gmail.com>
2024-03-03 19:45:03 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<div align="center">
+	<h1 align="center">Calculadora IMC</h1>
+	<p align="center">Aplicação desenvolvida num desafio de recriação de layout, lançado no curso Explorer 3.0 da Rocketseat no Stage 5, na qual foi disponibilizado um design no figma.</p>
+</div>
+<h3  align="center">
+		✅ Status: concluído ✅
+</h3>
+
+## :page_facing_up: Conteúdo
+
+- [Demonstração](#tv-demonstração)
+- [Tecnologias](#hammer_and_wrench-tecnologias)
+
+## :tv: Demonstração
+
+<p align="center">
+  <img alt="Demonstração" src="/readme/imcImage.png" width="800">
+</p>
+
+## :hammer_and_wrench: Tecnologias
+
+- [HTML](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+  - [Flexbox](https://developer.mozilla.org/pt-BR/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox)
+  - [GRID](https://developer.mozilla.org/en-US/docs/Web/CSS/grid)
+  - [MediaQueries](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries/Using_media_queries)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+  - [Modules](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules)
+
+## 📕License
+
+This project is under the [MIT](./LICENSE).
---
+++ js/main.js
@@ -0,0 +1,46 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { calculateIMC, isNotANumber } from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputWeight.focus()
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = Number(inputWeight.value)
+  const height = Number(inputHeight.value)
+
+  const weightOrHeightIsNotANumber =
+    isNotANumber(weight) || isNotANumber(height)
+
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open()
+    inputWeight.value = inputHeight.value = ''
+    inputWeight.focus()
+    inputHeight.value = inputWeight.value = ''
+    return
+  }
+
+  AlertError.close()
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+  inputHeight.value = inputWeight.value = ''
+
+  inputWeight.focus()
+  inputHeight.value = inputWeight.value = ''
+}
